3539d4a068a5ad9070235cec8e884fd13eee5d0a
rony <ronykb@yahoo.com>
2011-08-09 09:57:26 +0000

    from server

--- app/manifests/init.pp
+++ app/manifests/init.pp
@@ -16,13 +16,13 @@ class app::gitclone_app {
 }
 class app::dbcreate {
 	exec { "db-create":
-		command =>"/etc/puppet/modules/application/scripts/mysql-db-create.sh $application_mysql_dbname",
+		command =>"/etc/puppet/modules/app/scripts/mysql-db-create.sh $application_mysql_dbname",
 		require => Service["mysql"]
 }
 }
 class app::dbrestore {
 	exec { "db-restore":
-		command =>"/etc/puppet/modules/application/scripts/mysql-db-restore.sh $application_mysql_dbname $application_mysql_dump_location_for_dbrestore",
+		command =>"/etc/puppet/modules/app/scripts/mysql-db-restore.sh $application_mysql_dbname $application_mysql_dump_location_for_dbrestore",
 		require => Class ["app::dbcreate"],
 }
 }
@@ -46,7 +46,7 @@ class app::edit_for_cleanurl{
 }
 class app::edit_for_documentroot{
 	exec { "edit-documentRoot-folder-path":
-       command => "/etc/puppet/modules/application/scripts/edit-documentRoot-folder-path.sh $application_apache_default_documentroot $application_apache_current_documentroot",
+       command => "/etc/puppet/modules/app/scripts/edit-documentRoot-folder-path.sh $application_apache_default_documentroot $application_apache_current_documentroot",
       require => Package["apache2"]
 }
 }
