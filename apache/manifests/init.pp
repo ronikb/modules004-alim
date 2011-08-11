d05ac5b95f5b95d857b6cb66abb2d13e29c92318
Roni <roni@citrusinformatics.com>
2011-08-11 09:51:40 +0530

    merge install file content in init

--- apache/manifests/init.pp
+++ apache/manifests/init.pp
@@ -1,4 +1,18 @@
-# This is a core file of apache module
+#This will install apache2 packages in Ubuntu/Debian systems
+# TODO: make linux distro-independent (works only on Ubuntu/Debian now)
+class apache::install {
+	package { [ "apache2" ]:
+	ensure => present,
+}
+	service { "apache2":
+	ensure => running,
+	hasstatus => true,
+	hasrestart => true,
+	enable => true,
+	require => Package ["apache2"],
+}
+}
+
 class apache {
 	include apache::install 
 }
