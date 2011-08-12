589e10120e722ea34136326fa191970fc3cb40ec
Roni <roni@citrusinformatics.com>
2011-08-12 13:30:30 +0530

    updated creating db scripts

--- app/scripts/mysql-db-create.sh
+++ app/scripts/mysql-db-create.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 #This will create MySql database
 dbuser=root
-app_mysql_user=$1
-app_mysql_password=$2
-app_mysql_dbname=$3
+app_mysql_user="apliww"
+app_mysql_password="fiwwfa"
+app_mysql_dbname="fifaaww"
 #STEP 1 - CREATE DB
 #check if db exists first
 echo "Creating a database for $app_mysql_dbname"
-DBS=`mysql -u$dbuser -p$mysql_password -Bse 'show databases'| egrep -v 'information_schema|mysql'`
-for db in $DBS; do
-if [ "$db" = "$app_mysql_dbname" ]
-then
-echo "This database already exists : exiting now"
-    exit
-  fi
-done
-mysqladmin -u$dbuser -p$mysql_password create $app_mysql_dbname;
-echo "Database $mysql_dbname created"
+#DBS=`mysql -u$dbuser -p$mysql_password -Bse 'show databases'| egrep -v 'information_schema|mysql'`
+#for db in $DBS; do
+#if [ "$db" = "$app_mysql_dbname" ]
+#then
+#echo "This database already exists : exiting now"
+#    exit
+#  fi
+#done
+mysql -u root -h localhost -Bse "CREATE DATABASE $app_mysql_dbname"
+echo "Creating a database user and grant all privigages to $app_mysql_user"
 mysql -u root -h localhost -Bse "GRANT ALL PRIVILEGES ON $app_mysql_dbname.* TO '$app_mysql_user'@'localhost' IDENTIFIED BY '$app_mysql_password'"
+
+#mysqladmin -u$dbuser -p$mysql_password create $app_mysql_dbname;
+#echo "Database $mysql_dbname created"
+#mysql -u root -h localhost -Bse "GRANT ALL PRIVILEGES ON $app_mysql_dbname.* TO '$app_mysql_user'@'localhost' IDENTIFIED BY '$app_mysql_password'"
